c313bdfc9ec18643cebbef277a6dee78bd9ca91d
Ali Yaz <akyaz0261@gmail.com>
2025-05-16 16:43:24 +0300

    domain fix vite

--- vite.config.js
+++ vite.config.js
@@ -184,7 +184,7 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
-	base: '/testgo.biz/',
+	base: '/',
 	server: {
 		cors: true,
 		headers: {
