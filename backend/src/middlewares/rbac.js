49ec4795fcb24dcfa37cad7fc9bead7671f7fa3d
akyaz0261 <akyaz0261@gmail.com>
2025-05-20 13:20:35 +0000

    FIREBASE DEV

--- backend/src/middlewares/rbac.js
+++
@@ -1,7 +0,0 @@
-// req.user.roles içindeki rolleri kontrol eder
-exports.ensureAdmin = (req, res, next) => {
-  if (!req.user.roles.includes('admin')) {
-    return res.status(403).json({ error: 'Admin yetkisi gerekli' });
-  }
-  next();
-};
