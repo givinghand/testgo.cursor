53ff0c0ecbcb8c8f9f9811dddf74bca17b02cf65
givinghand <151047807+givinghand@users.noreply.github.com>
2025-05-22 17:50:43 +0300

    4

--- src/components/blog/ScoreCalculatorContent.jsx
+++ src/components/blog/ScoreCalculatorContent.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
